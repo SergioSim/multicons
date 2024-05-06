db76a96f0f0046a195283e514228e965f2dcbf48
SergioSim <Sergio.Simonian@etu.unice.fr>
2024-05-06 11:30:17 +0200

    ✅(tests) add test for consensus_function_15
    The `consensus_function_15` was missing a dedicated test.

--- tests/test_consensus.py
+++ tests/test_consensus.py
@@ -7,6 +7,7 @@ from multicons import (
     consensus_function_12,
     consensus_function_13,
     consensus_function_14,
+    consensus_function_15,
 )
 
 
@@ -94,4 +95,25 @@ def test_consensus_consensus_function_14(value, expected):
     """Tests the consensus_function_14 should produce the expected value."""
 
     consensus_function_14(value)
+    assert value == expected
+
+
+@pytest.mark.parametrize(
+    "value, expected",
+    [
+        # A unique cluster remains unique
+        ([{1}], [{1}]),
+        # Clusters that are subsets are removed
+        ([{1, 2}, {1}], [{1, 2}]),
+        ([{1, 2}, {1, 2, 3}], [{1, 2, 3}]),
+        # Clusters that are intersections with a ratio higher than MT are merged
+        ([{1, 2, 3}, {2, 3, 4}, {5}], [{1, 2, 3, 4}, {5}]),
+        # Clusters that are intersections with a ration lower than MT are split
+        ([{1, 2, 3}, {3, 4, 5}, {6}], [{1, 2, 3}, {4, 5}, {6}]),
+        ([{1, 2, 3, 4}, {4, 5, 6}, {7}], [{1, 2, 3}, {4, 5, 6}, {7}]),
+    ],
+)
+def test_consensus_consensus_function_15(value, expected):
+    """Tests the consensus_function_15 should produce the expected value."""
+    consensus_function_15(value)
     assert value == expected
